fa550447b0486651f3c8d3ac8a09c56874657e48
alexgrigoras <alexgrigoras>
2018-05-17 18:09:45 +0300

    Testele sunt functionale (1,2,3)

--- src/ro/tutoriale3d/webserver/servlet/TestServlet.java
+++ src/ro/tutoriale3d/webserver/servlet/TestServlet.java
@@ -11,33 +11,18 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriBuilder;
 import org.glassfish.jersey.client.ClientConfig;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringReader;
 import java.net.URI;
-import java.util.Enumeration;
-
-import javax.json.Json;
-import javax.json.JsonArray;
-import javax.json.JsonObject;
-import javax.json.JsonReader;
-import javax.json.stream.JsonParsingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.UriBuilder;
-
-import org.glassfish.jersey.client.ClientConfig;
 
 /**
- * Servlet implementation class TestServlet abcd
+ * Servlet implementation class TestServlet
  */
 @WebServlet("/TestServlet")
 public class TestServlet extends HttpServlet {
@@ -57,6 +42,8 @@ public class TestServlet extends HttpServlet {
 	    response.setContentType("text/html");
 	    response.setCharacterEncoding("utf-8");
 	    PrintWriter pw = response.getWriter();
+	    /*
+	    pw.append("Context parameters: ").append(request.getParameter("id")).append("<br/>");
 	    pw.append("<h1>Exemplu de servlet</h1>");
         pw.append("Context path: ").append(request.getContextPath()).append("<br/>");
         pw.append("Request URI: ").append(request.getRequestURI()).append("<br/>");
@@ -68,16 +55,18 @@ public class TestServlet extends HttpServlet {
             String attr = attributeNames.nextElement();
             pw.append("- ").append(attr).append(": ").append(request.getAttribute(attr).toString()).append("<br/>");
         }
-        
+        */
         ClientConfig config = new ClientConfig();
 		Client client = ClientBuilder.newClient(config);
 
 		WebTarget target = client.target(getBaseURI());
+		
+		String parameter = request.getParameter("id");
 
 		// api  -> numele specificat in fisierul web.xml de la server in interiorul tag-ului url-pattern
 		// tutorials -> numele specificat in fisierul Tutorials.java la adnotarea @Path
 
-		String jsonString = target.path("api").path("tests").request().accept(MediaType.APPLICATION_JSON).get(String.class);
+		String jsonString = target.path("api").path("tests").path(parameter).request().accept(MediaType.APPLICATION_JSON).get(String.class);
 		
 		jsonString = "{\"Intrebari\":" + jsonString + "}";
 		
@@ -86,15 +75,21 @@ public class TestServlet extends HttpServlet {
 			JsonObject obj = rdr.readObject();
 			JsonArray results = obj.getJsonArray("Intrebari");
 			
+			pw.append("<section class=\"first_container\">");
+			pw.append("<h2 align = \"center\">TEST "+ parameter + "</h2>");
+			pw.append("<ol class=\"answers\">");
+			
 			for (JsonObject result : results.getValuesAs(JsonObject.class)) {
-				pw.append("Nr_intrebare: " + result.getInt("nr_intrebare")).append("<br/>");
-				pw.append("Enunt: " + result.getString("enunt")).append("<br/>");
-				pw.append("Raspuns1: " + result.getString("rasp1")).append("<br/>");
-				pw.append("Raspuns2: " + result.getString("rasp2")).append("<br/>");
-				pw.append("Raspuns3: " + result.getString("rasp3")).append("<br/>");
-				pw.append("Raspuns4: " + result.getString("rasp4")).append("<br/>");
-				pw.append("Solutie: " + result.getString("solutie")).append("<br/>");
+		    	pw.append("<li><p class=\"question\">" + result.getString("enunt") + "</p>\n" +
+			    		"<div><input type=\"radio\" name=\"q" + result.getInt("nr_intrebare") + "value=\"a\" id=\"q1a\"><label for=\"q1a\">" + result.getString("rasp1") + "</label></div><br/>\n" + 
+			    		"<div><input type=\"radio\" name=\"q" + result.getInt("nr_intrebare") + "value=\"b\" id=\"q1b\"><label for=\"q1b\">" + result.getString("rasp2") + "</label></div><br/>\n" + 
+			    		"<div><input type=\"radio\" name=\"q" + result.getInt("nr_intrebare") + "value=\"c\" id=\"q1c\"><label for=\"q1c\">" + result.getString("rasp3") + "</label></div><br/>\n" + 
+						"<div><input type=\"radio\" name=\"q" + result.getInt("nr_intrebare") + "value=\"d\" id=\"q1d\"><label for=\"q1d\">" + result.getString("rasp4") + "</label></div><br/>\n" +
+			    		"</li>");
 			}
+			
+			pw.append("</ol>");
+			pw.append("<br/></section><br/><br/><br/><br/>");
 	         			
 		} catch (JsonParsingException e) {
 			 System.out.println("Json not valid!");
