fe703afafe957d455ca6cfd4a3214cd6899cc382
grigorasalex <alex_grigoras_10@yahoo.com>
2020-10-15 17:44:15 +0300

    Updated structure

--- etapa_3/creare_utilizator.sql
+++
@@ -1,4 +0,0 @@
-create table utilizator(ID int NOT NULL AUTO_INCREMENT, Nume_utilizator Varchar(30) NOT NULL, Email Varchar(30) NOT NULL,Parola Varchar(30) NOT NULL, PRIMARY KEY(ID));
-insert into utilizator(Nume_utilizator,Email,Parola) values('Utilizator1','utilizator_1@yahoo.com','utilizator1');
-insert into utilizator(Nume_utilizator,Email,Parola) values('Utilizator2','utilizator_2@yahoo.com','utilizator2');
-insert into utilizator(Nume_utilizator,Email,Parola) values('Utilizator3','utilizator_3@yahoo.com','utilizator3');
